09bc4a690278099a96863b2872fbc86b29bd5000
Prakash Banjade <prakashbanjade191@gmail.com>
2024-04-27 00:24:43 +0545

    type explicitely defined

--- graphql_express_apollo/src/users/users.service.ts
+++ graphql_express_apollo/src/users/users.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { User as UserType } from '../graphql'
 
 @Injectable()
 export class UsersService {
-  users = [
+  users: UserType[] = [
     {
       id: 1,
       name: 'John',

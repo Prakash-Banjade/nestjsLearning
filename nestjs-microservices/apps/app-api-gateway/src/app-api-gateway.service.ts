9fef9631e297a5d83ce7d136c27272655ec3adba
Prakash Banjade <prakashbanjade191@gmail.com>
2024-09-10 18:55:09 +0545

    remove microservices app

--- nestjs-microservices/apps/app-api-gateway/src/app-api-gateway.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppApiGatewayService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
--- nestjs-microservices/apps/app-api-gateway/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppApiGatewayModule } from './app-api-gateway.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppApiGatewayModule);
-  await app.listen(3000);
-}
-bootstrap();

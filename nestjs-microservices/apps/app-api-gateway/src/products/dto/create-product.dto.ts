3c79f28082e5516164424c15de89cf2d53b476a1
Prakash Banjade <prakashbanjade191@gmail.com>
2024-09-16 20:46:22 +0545

    add class-validator

--- nestjs-microservices/apps/app-api-gateway/src/products/dto/create-product.dto.ts
+++ nestjs-microservices/apps/app-api-gateway/src/products/dto/create-product.dto.ts
@@ -1,15 +1,24 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsInt, IsNotEmpty, IsNumber, IsString, Max, Min } from "class-validator";
 
 export class CreateProductDto {
     @IsString()
     @IsNotEmpty()
     name: string;
 
+    @IsNotEmpty()
+    @IsString()
     description: string;
 
+    @IsNotEmpty()
+    @IsNumber()
+    @Min(0)
     price: number;
 
+    @IsNotEmpty()
+    @IsInt()
     stock: number;
 
+    @IsNumber()
+    @Max(5)
     rating: number;
 }

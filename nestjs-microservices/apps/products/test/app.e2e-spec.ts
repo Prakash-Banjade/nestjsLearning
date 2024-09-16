02d10ac25d3a99c0cbd71ae3afc5aab9387888df
Prakash Banjade <prakashbanjade191@gmail.com>
2024-09-16 20:21:30 +0545

    fix: use correct module name

--- nestjs-microservices/apps/products/test/app.e2e-spec.ts
+++ nestjs-microservices/apps/products/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { ProductsModule } from '../src/products-app.module';
+import { ProductsAppModule } from '../src/products-app.module';
 
 describe('ProductsController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [ProductsModule],
+      imports: [ProductsAppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();

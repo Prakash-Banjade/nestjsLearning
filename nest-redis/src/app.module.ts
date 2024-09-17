db22feba871fdb6574c7cfe5c0f356c67fbbbcdd
Prakash Banjade <prakashbanjade191@gmail.com>
2024-09-17 23:00:15 +0545

    integrate cache-manager

--- nest-redis/src/app.module.ts
+++ nest-redis/src/app.module.ts
@@ -4,13 +4,26 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './config/db-config';
 import { User } from './users/users.entity';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService),
-    TypeOrmModule.forFeature([User])
+    TypeOrmModule.forFeature([User]),
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 60000,
+      max: 1000,
+    })
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule { }

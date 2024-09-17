7da09c3a51c2a24664f41dc44e022b38e0b19018
Prakash Banjade <prakashbanjade191@gmail.com>
2024-09-17 16:03:39 +0545

    setup db and user entity

--- nest-redis/src/app.module.ts
+++ nest-redis/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from './config/db-config';
+import { User } from './users/users.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(configService),
+    TypeOrmModule.forFeature([User])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- nest-redis/src/app.service.ts
+++ nest-redis/src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './users/users.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(User) private readonly usersRepo: Repository<User>
+  ) { }
+
+  getHello() {
+    
+    console.log('hello world')
+    
+    return this.usersRepo.find();
+  }
+
+  mockUsers = [
+    {
+      name: 'John',
+    },
+    {
+      name: 'Jane',
+    },
+    {
+      name: 'Joe',
+    }
+  ]
+
+  async setUsers() {
+    await this.usersRepo.save(this.mockUsers);
   }
 }
